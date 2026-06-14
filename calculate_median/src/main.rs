ea8cdc89824d93a0225ea90d62bfc7e9cc3d9bb8
blueniarb <blueniarb@gmail.com>
2026-06-14 11:19:03 +0800

    WIP: save before cleaning tracked files

---
+++ calculate_median/src/main.rs
@@ -0,0 +1,38 @@
+fn median(mut list: Vec<f32>) -> Option<f32> {
+    if list.is_empty() {
+        return None;
+    }
+    // Sort the array
+    list.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    match list.len() % 2 {
+        0 => Some((list[list.len() / 2] + list[(list.len() / 2) - 1]) / 2.0),
+        _ => Some(list[list.len() / 2]),
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::median;
+
+    #[test]
+    fn test_median_odd() {
+        let list: Vec<f32> = vec![1.0, 4.0, 5.0];
+        assert_eq!(median(list), Some(4.0));
+    }
+
+    #[test]
+    fn test_median_even() {
+        let list: Vec<f32> = vec![3.0, 1.5, 8.8, 5.0];
+        assert_eq!(median(list), Some(4.0));
+    }
+
+    #[test]
+    fn test_median_empty_input() {
+        let list: Vec<f32> = vec![];
+        assert_eq!(median(list), None);
+    }
+}
---
+++ calculate_median/target/debug/deps/calculate_median-8f7bcff66260ecc7.d
@@ -0,0 +1,9 @@
+/home/blueniarb/dev/rust/level_up_rust/calculate_median/target/debug/deps/calculate_median-8f7bcff66260ecc7.d: src/main.rs Cargo.toml
+
+/home/blueniarb/dev/rust/level_up_rust/calculate_median/target/debug/deps/libcalculate_median-8f7bcff66260ecc7.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
