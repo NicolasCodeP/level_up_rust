04035d4e2c032674f2df065b169650a86caaee2d
blueniarb <blueniarb@gmail.com>
2026-06-14 11:50:12 +0800

    Init find_unique_items

---
+++ find_unique_items/src/lib.rs
@@ -0,0 +1,28 @@
+// Extra Credit
+//
+// Generics:
+// Use Generics to accept Vec<T>
+// where T is type that implements Ord.
+//
+// Retain order:
+// Return elements in their original order.
+pub fn unique(list: Vec<i32>) -> Vec<i32> {
+    list
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_list_with_no_duplicates() {
+        let list = vec![1, 4, 5];
+        assert_eq!(unique(list), vec![1, 4, 5]);
+    }
+
+    #[test]
+    fn test_list_with_duplicates() {
+        let list = vec![1, 1, 3];
+        assert_eq!(unique(list), vec![1, 3]);
+    }
+}
