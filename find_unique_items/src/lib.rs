574424ff8080da9ac0782a9191a683de1dcabfed
blueniarb <blueniarb@gmail.com>
2026-06-14 16:33:38 +0800

    Complete find_unique_items

--- find_unique_items/src/lib.rs
+++ find_unique_items/src/lib.rs
@@ -1,3 +1,11 @@
+use std::collections::HashSet;
+
+//pub fn unique(mut list: Vec<i32>) -> Vec<i32> {
+//    list.sort_unstable();
+//    list.dedup();
+//    list
+//}
+
 // Extra Credit
 //
 // Generics:
@@ -6,10 +14,15 @@
 //
 // Retain order:
 // Return elements in their original order.
-pub fn unique<T: Ord>(mut list: Vec<T>) -> Vec<T> {
-    list.sort();
-    list.dedup();
-    list
+pub fn unique<T: Ord + std::hash::Hash + Copy>(list: Vec<T>) -> Vec<T> {
+    let mut new_list = vec![];
+    let mut set = HashSet::new();
+    for x in list {
+        if set.insert(x) {
+            new_list.push(x);
+        }
+    }
+    new_list
 }
 
 #[cfg(test)]
@@ -17,14 +30,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_list_with_no_duplicates() {
+    fn test_empty_list() {
+        let list: Vec<i32> = vec![];
+        assert_eq!(unique(list), vec![]);
+    }
+
+    #[test]
+    fn test_sorted_list() {
         let list = vec![1, 4, 5];
         assert_eq!(unique(list), vec![1, 4, 5]);
     }
 
     #[test]
-    fn test_list_with_duplicates() {
+    fn test_unsorted_list() {
+        let list = vec![4, 1, 5];
+        assert_eq!(unique(list), vec![4, 1, 5]);
+    }
+
+    #[test]
+    fn test_sorted_list_with_duplicates() {
         let list = vec![1, 1, 3];
         assert_eq!(unique(list), vec![1, 3]);
+    }
+
+    #[test]
+    fn test_unsorted_list_with_duplicates() {
+        let list = vec![3, 1, 1];
+        assert_eq!(unique(list), vec![3, 1]);
     }
 }
